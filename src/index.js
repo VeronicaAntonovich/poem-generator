c127cd7cd979b3a64c4f231cf644ff5accb33c2d
VeronicaAntonovich <veronica93antonovich@gmail.com>
2025-04-17 00:50:21 +0200

    changed to recipe generator

--- src/index.js
+++ src/index.js
@@ -13,13 +13,13 @@ function generatePoem(event) {
   let instructionsInput = document.querySelector("#user-instructions");
   let apiKey = "2ff7dtfa19c19dd1af4e21073fb09o3c";
   let context =
-    "You are a romantic Poem expert and love to write short poems. You mission is to generate a 4 line poem in basic HTML and separate each line with a <br />. Make sure to follow the user instructions. Do not include a title to the poem. Sign the poem with 'SheCodes AI' inside a <strong> element at the end of the poem and NOT at the beginning";
-  let prompt = `User instructions: Generate a French poem about ${instructionsInput.value}`;
+    "You are a chief and love to cook. You mission is to generate a short recipe in basic HTML and separate each line with a <br />. Make sure to follow the user instructions. Sign the recipe with 'SheCodes AI' inside a <strong> element at the end of the poem and NOT at the beginning. Do not show html at the beginning. Use <strong> element for Ingredients and Instractions. Include the title of the recipe.";
+  let prompt = `User instructions: Generate recipe with ${instructionsInput.value}`;
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   let poemElement = document.querySelector("#poem");
   poemElement.classList.remove("hidden");
-  poemElement.innerHTML = `<div class="generating">⏳ Generating a French poem about ${instructionsInput.value}</div>`;
+  poemElement.innerHTML = `<div class="generating">⏳ Generating a recipe including ${instructionsInput.value}</div>`;
 
   axios.get(apiURL).then(displayPoem);
 }
